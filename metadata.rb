8704f4a1cefa8aa962f4d704cb7d87371daff940
Julien Berard <jujugrrr@gmail.com>
2014-11-28 11:04:38 +0000

    First commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# Encoding: utf-8
+name             'mysql2'
+maintainer       'Rackspace'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures mysql2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
+
+depends 'mysql'
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: mysql2
+# Recipe:: default
+#
+# Copyright 2014, Rackspace
+#
+
+log 'YEEAAAHHHHH'
